ae5db3124ef71acb8e07b730c19e0e690e7507db
jerdie <5143814+jerdie@users.noreply.github.com>
2021-11-11 01:10:06 +0800

    new setup beer controller add test

---
+++ src/main/java/guru/springframework/msscbeerservice/controller/BeerController.java
@@ -0,0 +1,28 @@
+package guru.springframework.msscbeerservice.controller;
+
+import guru.springframework.msscbeerservice.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public BeerDto getBeer(@PathVariable UUID beerId){
+        return BeerDto.builder().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<BeerDto> create(@RequestBody BeerDto beerDto){
+        return new ResponseEntity<>(beerDto, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable UUID beerId, @RequestBody BeerDto beerDto){
+    }
+}
---
+++ src/main/java/guru/springframework/msscbeerservice/model/BeerDto.java
@@ -0,0 +1,31 @@
+package guru.springframework.msscbeerservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastUpdatedDate;
+
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+}
